ea332c6b18f59cbb0e947ba75032f5ac94d9bfd9
Saugatpdsn <saugatpudasaini63@gmail.com>
2024-12-18 09:28:51 +0545

    Add files via upload

---
+++ celcius to fahenheit using constructot.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+//c=F;
+using namespace std;
+class faherenheit
+{
+private:
+    float f;
+public:
+    faherenheit()
+    {
+        f=0;
+
+    }
+    faherenheit(float x)
+    {
+        f=x;
+    }
+    float returnfaherenheit()
+    {
+        return f;
+    }
+    void display()
+    {
+        cout<< "faherenheit="<<f<<endl;
+    }
+
+};
+class celcius
+{
+    private
+};
